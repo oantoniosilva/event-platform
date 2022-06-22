dedcd6bdf632dc11aca3a1fd1c03435a1cbf8e26
Antonio <antoniopsilva083@gmail.com>
2022-06-22 01:18:08 -0300

    feat: create lesson component style

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,24 @@
 export function Lesson() {
-  return <h1>Lesson</h1>
+  return (
+    <a href="#">
+      <span className="text-gray-300">
+        Domingo • 20 de junho • 19h00
+      </span>
+
+      <div className="rounded border border-gray-400 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium">
+            Conteúdo liberado
+          </span>
+          <span className="text-xs rounded py-[2px] px-2 border border-green-300 text-white font-bold">
+            AO VIVO
+          </span>
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">
+          Abertura do evento Ignite Lab
+        </strong>
+      </div>
+    </a>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
       colors: {
         green: {
           300: '#00B37E',

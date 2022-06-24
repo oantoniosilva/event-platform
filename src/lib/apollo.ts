8b0ecb46ea84625727160611e06cb6c9660086a6
Antonio <antoniopsilva083@gmail.com>
2022-06-24 00:46:59 -0300

    feat: create video component and styles

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache} from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nmsmd00gh001xo3x2ggljz/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4qgyp413w3n01ywauhf8gvm/master',
   cache: new InMemoryCache()
 });
